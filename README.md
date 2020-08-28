ec727a996c7f0d023269a6fb8e65d906b6201e84
Edgaras <edgaras@Edgarass-MacBook-Pro.local>
2020-08-28 21:24:15 +0300

    Update swagger docs

--- README.md
+++ README.md
@@ -6,4 +6,7 @@
 - Install composer dependencies: `docker exec -it tesonet_web composer install`
 - Run migration `docker exec -it tesonet_web php artisan migrate`
 - After that, run `docker exec -it tesonet_web php artisan db:seed --class=UserSeeder` to have some users preloaded into the database.
+- After that, run `docker exec -it tesonet_web php artisan passport:install` to have some users preloaded into the database.
 - Use some HTTP client (Postman) to test API requests (view swagger documentation [http://localhost:8080/api/docs](http://localhost:8080/api/docs))
+
+System uses external prices exchange API from [https://www.cryptonator.com/api/](https://www.cryptonator.com/api/).
--- app/Http/Controllers/API/ConvertController.php
+++ app/Http/Controllers/API/ConvertController.php
@@ -6,6 +6,57 @@ use App\Asset;
 use App\Service\Converter;
 use Illuminate\Http\JsonResponse;
 
+/**
+ * @OA\Get(
+ * path="/assets/convert/usd",
+ * summary="Returns user's assets value in USD",
+ * description="Returns user's assets value in USD",
+ * operationId="convertAssets",
+ * tags={"convertAssets"},
+ * security={ {"bearer": {} }},
+ * @OA\Response(
+ *    response=200,
+ *    description="Success",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="success", type="bool", example="true"),
+ *       @OA\Property(property="data", type="object"),
+ *       @OA\Property(property="message", type="string", example="Conversion successful.")
+ *    )
+ * ),
+ * @OA\Response(
+ *    response=401,
+ *    description="User should be authorized to get conversion information",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="Unauthenticated"),
+ *    )
+ * )
+ * ),
+ * @OA\Get(
+ * path="/assets/convert/{id}/usd",
+ * summary="Returns user's asset value in USD",
+ * description="Returns user's asset value in USD",
+ * operationId="convertAsset",
+ * tags={"convertAssets"},
+ * security={ {"bearer": {} }},
+ * @OA\Response(
+ *    response=200,
+ *    description="Success",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="success", type="bool", example="true"),
+ *       @OA\Property(property="data", type="object"),
+ *       @OA\Property(property="message", type="string", example="Conversion successful.")
+ *    )
+ * ),
+ * @OA\Response(
+ *    response=401,
+ *    description="User should be authorized to get conversion information",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="Unauthenticated"),
+ *    )
+ * )
+ * ),
+ *
+
 class ConvertController extends BaseController
 {
     private Converter $converter;
